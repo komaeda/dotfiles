2a0e9c79445dfeda868f5edfd4afe96dbdd130fe
olivia <olivia@fastmail.com>
2018-02-08 11:28:44 +0100

    add anaconda and docker compose shortcuts

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 set PATH ~/.rbenv/bin $PATH
 status --is-interactive; and source (rbenv init -|psub)
 set PATH ~/.cargo/bin $PATH
+set PATH /usr/local/anaconda3/bin $PATH
 set PATH ~/n/bin $PATH
 
 alias editconfig "vim ~/dotfiles/fish/.config/fish/config.fish; and source ~/.config/fish/config.fish"
@@ -10,7 +11,7 @@ alias t "tree . -L 3"
 alias l "git log --color --graph --abbrev-commit --oneline"
 alias q "g add --all; and g commit; and g push"
 alias be "bundle exec"
-alias z "sudo zypper"
+alias doc "docker-compose"
 
 if command --search hub > /dev/null do
   alias git "hub"
