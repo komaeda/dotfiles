fb168b90fe99da89a06fd8e750f8068c99cab354
olivia <olivia@fastmail.com>
2018-09-18 11:39:16 +0200

    more emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,11 +7,16 @@
       scroll-step 1
       make-backup-files nil)
 (setq-default indent-tabs-mode nil)
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
 (global-hl-line-mode 1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (auto-compression-mode t)
+
+(if window-system
+    (load-theme 'solarized-light t))
 
 (set-terminal-coding-system 'iso-latin-1)
 
@@ -19,6 +24,8 @@
       '((font . "Fira Code Medium 13")))
 (setq initial-frame-alist
       '((font . "Fira Code Medium 13")))
+
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; IVY CONFIGURATION
 
@@ -33,7 +40,8 @@
 
 (setq telephone-line-primary-right-separator 'telephone-line-abs-left
       telephone-line-secondary-right-separator 'telephone-line-abs-hollow-left
-      telephone-line-height 22)
+      telephone-line-height 22
+      telephone-line-evil-use-short-tag t)
 (telephone-line-mode 1)
 
 ;; ZOOM CONFIGURATION
@@ -43,3 +51,8 @@
 ;; EVIL CONFIGURATION
 
 (evil-mode 1)
+
+;; MAGIT CONFIGURATION
+
+(global-set-key (kbd "C-x g") 'magit-status)
+(require 'evil-magit)
