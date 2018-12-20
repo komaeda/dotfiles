020770c9899ddd47bf74bc9bf178a0bba4ec003b
liv <45509655+komaeda@users.noreply.github.com>
2018-12-20 21:52:10 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-stow -t ~ -S bin/ emacs/ feh/ fish/ git/ i3/ kitty/ nvim/ polybar/ redshift/
-splatmoji/ xorg/
+stow -t ~ -S bin/ emacs/ feh/ fish/ git/ i3/ kitty/ nvim/ polybar/ redshift/ splatmoji/ xorg/
 
 rm -rf /etc/nixos/*
 stow -t /etc/nixos -S nixos/
