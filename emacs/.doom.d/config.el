e9b867722cd27e02a68c42bbffe0c5e72373c729
marisa <marisa@mokou.zone>
2019-07-31 21:45:52 +0200

    Use custom font for Emacs

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -2,3 +2,10 @@
 
 ;; Maximize the window upon startup
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Use Input Mono as default font
+(setq doom-font (font-spec :family "Input Mono" :size 14))
+
+;; Increase font size on MacOS
+(if (eq system-type 'darwin)
+    (setq doom-font (font-spec :family "Input Mono" :size 18)))
