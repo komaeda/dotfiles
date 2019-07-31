01cabd492d771845011bdd56b1436190e2e4f2c3
marisa <marisa@mokou.zone>
2019-07-31 15:27:36 +0200

    Don't use diff-so-fancy

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,4 +1,4 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+;; Maximize the window upon startup
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
